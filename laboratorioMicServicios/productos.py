c9f5020b9b2810c06f3a9b6c109dafaa6b64dd45
jalopezqv <jalopezqvision@gmail.com>
2022-01-05 18:23:23 -0500

    Se realiza el laboratorio para la creacion de un microservicio con los metodos basicos GET, POST, PUT y DELETE en python con Flask.

---
+++ laboratorioMicServicios/productos.py
@@ -0,0 +1,6 @@
+productos = [
+    {"nombre":"laptop", "precio":800, "cantidad":4},
+    {"nombre":"mouse", "precio":40, "cantidad":10},
+    {"nombre":"teclado", "precio":50, "cantidad":8},
+    {"nombre":"monitor", "precio":400, "cantidad":2},
+]
